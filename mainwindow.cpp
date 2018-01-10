48d83a56da269f5c2009be39df5b8227b063c917
wojciechjmaciejewski <xardas663@gmail.com>
2018-01-10 08:57:46 +0100

    third commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,58 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+ui->textBrowser->clear();
+ui->textBrowser->append("Witaj po raz drugi");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+   text = ui->textEdit->toPlainText();
+   ui->textBrowser->clear();
+
+   ui->textBrowser->append("Witaj "+text);
+}
+
+void MainWindow::on_radioButton_toggled(bool checked)
+{
+       ui->textBrowser->setTextColor((Qt::red));
+}
+
+void MainWindow::on_radioButton_2_toggled(bool checked)
+{
+       ui->textBrowser->setTextColor((Qt::green));
+}
+
+void MainWindow::on_radioButton_3_toggled(bool checked)
+{
+       ui->textBrowser->setTextColor((Qt::blue));
+}
+
+void MainWindow::on_verticalSlider_valueChanged(int value)
+{
+    if(value>50)
+    {
+
+        text = ui->textEdit->toPlainText();
+        ui->textBrowser->clear();
+
+        ui->textBrowser->append("Cześć "+text);
+    }
+    if(value>80)
+    {
+        text = ui->textEdit->toPlainText();
+        ui->textBrowser->clear();
+
+        ui->textBrowser->append("Dzień dobry "+text);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<QString>
 
 namespace Ui {
 class MainWindow;
@@ -13,7 +14,22 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    QString text;
     ~MainWindow();
+
+
+private slots:
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_radioButton_toggled(bool checked);
+
+    void on_radioButton_2_toggled(bool checked);
+
+    void on_radioButton_3_toggled(bool checked);
+
+    void on_verticalSlider_valueChanged(int value);
 
 private:
     Ui::MainWindow *ui;
